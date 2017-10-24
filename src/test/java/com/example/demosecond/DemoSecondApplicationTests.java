e37a6b969eabede5a6afdf5d4f1b9b21468ac905
Kasun Kariyawasam <kasunsk@gmail.com>
2017-10-24 17:50:28 +0530

    first commit

---
+++ src/test/java/com/example/demosecond/DemoSecondApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demosecond;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSecondApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
